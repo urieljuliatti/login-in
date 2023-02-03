b4e587e786b2dd43e3e57f422f7ad01e0dcb3584
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-03 16:36:42 -0300

    Add before each for rspec

--- spec/controllers/bands_controller_spec.rb
+++ spec/controllers/bands_controller_spec.rb
@@ -5,6 +5,11 @@ require 'rails_helper'
 RSpec.describe BandsController, type: :request do
   let(:band) { create(:band) }
 
+  before(:each) do
+    admin = FactoryBot.create(:user, :admin)
+    @header = { 'Authorization' => token(admin) }
+  end
+
   describe 'GET /bands' do
     context 'when the request hasnt an authenticathed user' do
       it 'should return unauthorized' do
@@ -15,8 +20,7 @@ RSpec.describe BandsController, type: :request do
 
     context 'when the request has an authorized user' do
       it 'lists the bands' do
-        admin = FactoryBot.create(:user, :admin)
-        get bands_path, headers: { 'Authorization' => token(admin) }
+        get bands_path, headers: @header
         expect(response).to have_http_status(:ok)
       end
     end
@@ -25,12 +29,12 @@ RSpec.describe BandsController, type: :request do
   describe 'GET /band/1' do
     context 'when the request has an authorized user' do
       it 'show the band' do
-        admin = FactoryBot.create(:user, :admin)
         band = create(:band)
-        get bands_path(band.id), headers: { 'Authorization' => token(admin) }
+        get bands_path(band.id), headers: @header
+        json_response = JSON.parse(response.body)[0]
         expect(response).to have_http_status(:ok)
         # need to be improved with https://github.com/collectiveidea/json_spec
-        # expect(json_response).to be eql(band_json)
+        # expect(json_response['id']).to be eql(JSON.parse(band.to_json)['id'])
       end
     end
   end
