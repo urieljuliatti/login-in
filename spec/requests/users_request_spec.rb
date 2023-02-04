59ad231d9aaa882b5607aa900557e74211082af7
Uriel Juliatti Valle <uriel.juliattivalle@gmail.com>
2023-02-04 07:50:33 -0300

    Listing users as admin

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /login' do
+  describe 'POST /login' do
     context 'when the request hasnt an authenticathed user' do
       it 'should return unauthorized' do
         post login_path
@@ -20,6 +20,27 @@ RSpec.describe 'Users', type: :request do
 
       it 'logs in the user' do
         post login_path, params: { username: 'urielvalle', password: '12345' }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'GET index/' do
+    before(:each) do
+      @admin = create(:user, :admin)
+      @header = { 'Authorization' => token(@admin) }
+    end
+
+    context 'when the request hasnt an authenticathed user' do
+      it 'should return unauthorized' do
+        get users_path
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+
+    context 'when the request has an authorized user' do
+      it 'lists the users' do
+        get users_path, headers: @header
         expect(response).to have_http_status(:ok)
       end
     end
